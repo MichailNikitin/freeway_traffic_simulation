fad7006dbce44479d6babc7c2c311636e8230264
Miniki_Al <miniki2002@yandex.ua>
2022-06-20 00:51:41 +0500

    Add first traffic(dont work)

--- InNuklear/temp/temp.c
+++ InNuklear/temp/temp.c
@@ -12,12 +12,25 @@
 #define HEIDTH_BUTTON 80
 #define HEIDTH_TEXT 100
 
-int running = 1;
-int FreeSpaceInStart = 1;
-int Collision = 0;
-int CarsCount = 0;
+#define i_prev(i,CarMax) (((i)==(0))?(CarMax):(i-1))
+#define i_next(i,CarMax) (((i)==(CarMax))?(0):(i+1))
 
-struct nk_canvas canvas;
+int running = 1; // is programm working (bool)
+int k = 3; // the amount of scaling (int, meters in 1 pixel)
+int FreeSpaceInStart = 1; //is there a free space in the start (bool)
+int Collision = 0;
+int CarsCount = 0; // Number of cars on the road (int)
+int CarsCountMax;
+
+double MaxSpeed_KmH = 5.0;
+int v_KmH = 0;
+int v_pixel = 0;
+int SafeDist_m = 10;
+int SafeDist_pixel = 0;
+double Accel = 2;
+int LastCar_index = 0
+
+                    struct nk_canvas canvas;
 struct nk_context *ctx;
 
 enum page {
@@ -94,73 +107,27 @@ canvas_end(struct nk_context *ctx, struct nk_canvas *canvas) {
    ctx->style.window.fixed_background = canvas->window_background;
 }
 
-/*________window:Start-Menu__________*/
-void menu() {
-   if (nk_begin(ctx, "Меню", nk_rect(0, 0, gdi.width, gdi.height),
-                NK_WINDOW_BORDER))
-   {
-      nk_layout_row_dynamic(ctx, HEIDTH_TEXT, 1);
-      nk_label(ctx, "Программа моделирования движения на автостраде", NK_TEXT_CENTERED);
-      nk_layout_row_dynamic(ctx, HEIDTH_BUTTON, 1);
-      if (nk_button_label(ctx, "Моделирование")) {
-         selected_page = MODEL;
-      }
-      nk_layout_row_dynamic(ctx, HEIDTH_BUTTON, 1);
-      if (nk_button_label(ctx, "О программе")) {
-         selected_page = ABOUT;
-      }
-      nk_layout_row_dynamic(ctx, HEIDTH_BUTTON, 1);
-      if (nk_button_label(ctx, "Закрыть")) {
-         running = 0;
-      }
+int moving_car_in(int i, int roadW) {
+   int new_x;
+   double t = clock() - cars[i].t;
+   v_KmH = cars[i].v;
+   v_pixel = (int)(v_KmH/3.6)/k;
+   new_x = cars[i].x + v_pixel*t + Accel*t*t/2;
+   if (new_x < roadW) {
+      if (cars[i].x - new_x < SafeDist_pixel)
+         return new_x;
+      else
+         return cars[i].x + v_pixel *t - Accel *t *t/2;
    }
-   nk_end(ctx);
+   else{
+      if 
+      }
 }
 
-/*______window:Program-description______*/
-void about() {
-   if (nk_begin(ctx, "О программе", nk_rect(0, 0, gdi.width, gdi.height),
-                NK_WINDOW_BORDER))
-   {
-      nk_layout_row_dynamic(ctx, HEIDTH_TEXT, 1);
-      nk_label(ctx, "Это Программа моделирования движения на автостраде", NK_TEXT_CENTERED);
-
-      nk_layout_row_dynamic(ctx, 250, 1);
-      nk_label(ctx, "Программа по моделированию движения автомобилей на автостраде, сдаланная бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-бла-", NK_TEXT_CENTERED);
-
-      nk_layout_row_dynamic(ctx, HEIDTH_BUTTON, 2);
-      if (nk_button_label(ctx, "Назад")) {
-         selected_page = MENU;
-      }
-      if (nk_button_label(ctx, "Закрыть")) {
-         running = 0;
-      }
+void add_car_in_start(int i_car){
+   if(cars[LastCar_index].x < SafeDist_pixel){
+      printf("dont have space in the beginning of the road");
+      return;
    }
-   nk_end(ctx);
-
-}
-
-/*______window:Modeling______*/
-void model() {
-   static int is_start = 0;
-
-   static double MaxSpeed = 5.0;
-   static int SafeDist = 10;
-   static double Accel = 2;
-
-   int roadH = 100;
-   int roadW = gdi.width;
-
-   time_t t;
-   srand((unsigned) time(&t));
-
-   static int motion_X = 10;
-   /*__drow-road&cars__*/
-   {
-      canvas_begin(ctx, &canvas, 0, 0, 0, roadW, roadH+20, nk_rgb(250,20,20));
-      nk_fill_rect(canvas.painter, nk_rect(0, 10, roadW,roadH), 1, nk_rgb(200, 200, 200));
-
-      /*_motion-cars_*/
-      if (is_start) {
-         int i = 0;
-         int v = _abracadabra_cast(cars[i]);
+   else{
+      _abracadabra_cast(cars[i_prev(i_car, CarsCountMax)]);
